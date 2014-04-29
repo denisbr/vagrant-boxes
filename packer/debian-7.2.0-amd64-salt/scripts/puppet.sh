dfdf06f6a9c60afc63759623a07ca9a2d11049fe
Denis <denis@startsiden.no>
2014-04-29 14:08:34 +0200

    7.5.0

--- packer/debian-7.2.0-amd64-salt/scripts/puppet.sh
+++
@@ -1,2 +0,0 @@
-# Install puppet
-sudo gem install puppet
--- packer/debian-7.5.0-amd64/scripts/ruby.sh
+++ packer/debian-7.5.0-amd64/scripts/ruby.sh
@@ -1,8 +1,10 @@
+#!/bin/bash -eux
+
 # Install Ruby from packages
-apt-get -y --force-yes install ruby ruby-dev libopenssl-ruby1.8 irb ri rdoc
+apt-get -y install unzip ruby1.9.1 ruby1.9.1-dev libruby1.9.1
 
 # Install Rubygems from source
-rg_ver=1.8.22
+rg_ver=2.2.2
 curl -o /tmp/rubygems-${rg_ver}.zip \
   "http://production.cf.rubygems.org/rubygems/rubygems-${rg_ver}.zip"
 (cd /tmp && unzip rubygems-${rg_ver}.zip && \
