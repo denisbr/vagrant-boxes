d56c14d83c8250dbf0fd37e8b6ca7fb21441da41
Denis <denis@startsiden.no>
2014-11-17 14:57:02 +0100

    Restructure as of upstreams new layout, to make merging possible.

--- packer/scripts/salt.sh
+++ packer/scripts/salt.sh
@@ -1,7 +1,10 @@
 #!/bin/bash -eux
 
+# codename of distro
+LSB_RELEASE=$(lsb_release -sc)
+
 wget -q -O- "http://debian.saltstack.com/debian-salt-team-joehealy.gpg.key" | apt-key add -
-echo "deb http://debian.saltstack.com/debian wheezy-saltstack main" > /etc/apt/sources.list.d/saltstack.list
+echo "deb http://debian.saltstack.com/debian ${LSB_RELEASE}-saltstack main" > /etc/apt/sources.list.d/saltstack.list
 apt-get update -qq
 apt-get install -q -y --force-yes salt-minion
 update-rc.d -f salt-minion remove
